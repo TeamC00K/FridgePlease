74e1f7f66fe3d95e01b2db906e582d4af20d93d9
vesselofgod <vesselofgod@naver.com>
2022-05-24 01:18:21 +0900

    feat: backend 라우터 분리

--- flask-server/model.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
