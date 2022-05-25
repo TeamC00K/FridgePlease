0bd09a8f25fe64aa6031d96d1a4f841f2538e4cb
vesselofgod <vesselofgod@naver.com>
2022-05-25 17:13:03 +0900

    feat: login 기능 구현

--- flask-server/auth.py
+++ flask-server/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, Response
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
+import json
 from models import User
 from app import db
 
@@ -10,13 +11,22 @@ auth = Blueprint('auth', __name__)
 def login():
     userid = request.json['id']
     password = request.json['passwd']
+    re = request.form.get('remember')
+    remember = True if request.form.get('remember') else False # 이 부분에 대해서 remember라는 값을 어디서 가져오는지 확인해봐야 함.
 
     user = User.query.filter_by(id=userid).first()
+    print(user.id, user.name)
     if not user or not check_password_hash(user.password, password):
-        return "not login"
-    print(userid, password)
-    res = Response("login")
-    return res
+        print("login fail")
+        res = Response(status=401)
+        return res
+    data = {}
+    data["name"] = user.name
+    data["id"] = user.id
+    login_user(user, remember=remember)
+    print("login success")
+    response = Response(json.dumps(data), status=200)
+    return response
 
 @auth.route('/register', methods=['POST'])
 def signup_post():
@@ -24,16 +34,14 @@ def signup_post():
     name = request.json['name']
     userid = request.json['id']
     password = request.json['passwd']
-    password = generate_password_hash(password, method='sha256')
-    user = User.query.filter_by(userid=userid).first() # if this returns a user, then the email already exists in database
-    print("userdata : ",user)
-    print(type(userid),type(password),type(name))
+    user = User.query.filter_by(id=userid).first() # if this returns a user, then the email already exists in database
+
     if user: # if a user is found, we want to redirect back to signup page so user can try again
         print("fail")
         return "fail"
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(userid = userid, password = password ,name=name)
+    new_user = User(id = userid, password = generate_password_hash(password, method='sha256') ,name=name)
 
     # add the new user to the database
     db.session.add(new_user)
@@ -43,4 +51,5 @@ def signup_post():
 
 @auth.route('/logout')
 def logout():
+    logout_user()
     return 'Logout'
