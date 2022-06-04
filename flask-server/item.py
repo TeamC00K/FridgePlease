af9c02c1bcad9c1a57efeb4157cfddd0a20336a8
vesselofgod <vesselofgod@naver.com>
2022-06-04 15:47:03 +0900

    feat: 현재 식재료 가져오는 api 구현

--- flask-server/item.py
+++ flask-server/item.py
@@ -1,23 +1,36 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
 from app import db
 from models import Item, ItemPicture, Category
 from datetime import datetime
 
 item = Blueprint('item', __name__)
 
-@item.route('/item/get', methods=['POST'])
-def item_get():
+@item.route('/item/add', methods=['POST'])
+def addItem():
     #식료품 구매 시 넘겨받을 정보들 
     itemId = request.json['itemId']
     userId = request.json['userId']
     category = request.json['category']
-    getCategory = Category.query.filter_by(category=category).first()
+    itemCategory = Category.query.filter_by(category=category).first()
     mfgDate = datetime.now()
 
-    new_item = Item(itemId = itemId, userId = userId, mfgDate=mfgDate, expDate = mfgDate + getCategory.expDate, countable = getCategory.countable, consumptionRate = 1)
+    new_item = Item(itemId = itemId, userId = userId, mfgDate=mfgDate, expDate = mfgDate + itemCategory.expDate, countable = itemCategory.countable, consumptionRate = 1)
 
     # add the new item to the database
     db.session.add(new_item)
     db.session.commit()
     print("new_item input success")
     return Response(status=200)
+
+
+@item.route('/item/get')
+def getItem():
+    userId = request.json['userId']
+    category = request.json['category']
+    getItems = Category.query.filter_by(userId=userId, category = category)
+    return jsonify(getItems)
+
+
+@item.route('/item/update', methods=['POST'])
+def updateItem():
+    pass
