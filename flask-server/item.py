2ff1a1e12398ca8ddfb6a43d4118d670d1472c19
vesselofgod <vesselofgod@naver.com>
2022-06-06 03:37:29 +0900

    docs  : Item 관련 api 주석 추가

--- flask-server/item.py
+++ flask-server/item.py
@@ -25,6 +25,11 @@ S3 = boto3.resource(
 
 
 def getCropImage(img, cor):
+    '''
+    전체 이미지에서 발견된 객체만 잘라주는 함수
+    parameters:
+        img:PIL.Image, cor:list (사진을 자를 좌표를 담음,(startX,startY,endX,endY))
+    '''
     detectcnt = len(cor[0])
     img_list = []
     for i in range(detectcnt):
@@ -37,17 +42,19 @@ def getCropImage(img, cor):
 @item.route('/item/add', methods=['POST'])
 def addItem():
     """
-    식료품 구매 시 넘겨받을 정보들
-    수량도 어지간하면 넘겨줬으면 함
+    식료품 구매 시 넘겨받은 정보들을 DB에 저장하는 함수
+    수량도 어지간하면 넘겨줬으면 함. 카테고리는 구매페이지 랜더링할 때 넘겨줄 예정
+    req: userId, category, subCategory, totalVol (수량)
+    res: Response(status=200) 등으로 성공, 실패여부를 status로 구분
     """
     #식료품 구매 시 넘겨받을 정보들 
-    itemId = request.json['itemId']
     userId = request.json['userId']
     category = request.json['category']
+    subCategory = request.json['subCategory']
     itemCategory = Category.query.filter_by(category=category).first()
     mfgDate = datetime.now()
 
-    new_item = Item(itemId = itemId, userId = userId, mfgDate=mfgDate, expDate = mfgDate + itemCategory.expDate, countable = itemCategory.countable, consumptionRate = 1)
+    new_item = Item(userId = userId, mfgDate=mfgDate, expDate = mfgDate + itemCategory.expDate, countable = itemCategory.countable, consumptionRate = 1)
 
     # add the new item to the database
     db.session.add(new_item)
@@ -58,6 +65,12 @@ def addItem():
 
 @item.route('/item/get')
 def getItem():
+    """
+    카테고리 페이지를 열어서 저장된 식료품 정보를 불러오는 함수
+    조만간 /category/fruit 이런 식으로 바꿀 예정
+    req: userId, category
+    res: 해당 카테고리에 해당되는 식료품 정보 : json list
+    """
     userId = request.json['userId']
     category = request.json['category']
     getItems = Category.query.filter_by(userId=userId, category = category)
@@ -66,6 +79,12 @@ def getItem():
 
 @item.route('/item/newimage', methods=['POST'])
 def addnewItem():
+    """
+    냉장고 사진을 받아오면 사진에 있는 식료품을 인식/분류하여 DB에 저장하는 함수
+    미완성 - 모델 적용까지 완료
+    req: dataForm - file에 이미지 전송
+    res: {이미지가 서버에 저장된 주소값, 인식된이미지 좌표값, 라벨(이름, 카테고리, 서브카테고리), 예상 유통기한}
+    """
     image = request.files['file']
     # userId = request.json['userId']
     filename = image.filename.split(".")[0]
