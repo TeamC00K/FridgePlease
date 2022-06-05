5968d5f597899f32b78cc2c014a2eb6f86015961
vesselofgod <vesselofgod@naver.com>
2022-06-06 03:19:26 +0900

    feat: 이미지 인식/분류모델과 flask server 연동

--- flask-server/item.py
+++ flask-server/item.py
@@ -23,6 +23,17 @@ S3 = boto3.resource(
     config=Config(signature_version='s3v4')
 )
 
+
+def getCropImage(img, cor):
+    detectcnt = len(cor[0])
+    img_list = []
+    for i in range(detectcnt):
+        # area = (startX,startY,endX,endY)
+        area = (cor[0][i],cor[2][i],cor[1][i],cor[3][i])
+        img_list.append(img.crop(area))
+    return img_list
+
+
 @item.route('/item/add', methods=['POST'])
 def addItem():
     """
@@ -56,12 +67,33 @@ def getItem():
 @item.route('/item/newimage', methods=['POST'])
 def addnewItem():
     image = request.files['file']
+    # userId = request.json['userId']
     filename = image.filename.split(".")[0]
     temp = Image.open(image)
+    cors = setup.Detection(temp)
+    cropped_imgs = getCropImage(temp, cors)
+    category_list = []
+    
+    for i in range(len(cropped_imgs)):
+        cropped_imgs[i].save(str(i)+".jpg")
+        category = setup.Classification(temp)
+        category_list.append(category)
     temp.save("temp.jpg")
     img = open("temp.jpg", 'rb')
-    cors = setup.Detection(temp)
+    
     S3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=img, ContentType='image/jpg')
+    #crop하는 과정 추가.
+    # 커태고리 분류
+    # db.session.execute("INSERT INTO Category (category,subCategory,expDate,countable,imgKey) values('fruit', 'apple', 30 , true ,'apple')")
+    mfgDate = datetime.now()
+    print("좌표: ",cors)
+    #itemCategory = Category.query.filter_by(category=category).first()
+    #new_item = Item(userId = userId, mfgDate=mfgDate, expDate = mfgDate + itemCategory.expDate, countable = itemCategory.countable, consumptionRate = 1)
+
+    # add the new item to the database
+    #db.session.add(new_item)
+    #db.session.commit()
+
     return jsonify(cors)
 
 
@@ -72,3 +104,5 @@ def updateItem():
 @item.route('/item/delete', methods=['POST'])
 def deleteItem():
     pass
+
+
